ac3da8b2e6ef22857c5f635b75132d5d44038702
Pavel Khegay <pashulia.pk@gmail.com>
2024-06-12 19:20:08 +0500

    add banners

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -31,16 +31,16 @@ const Login: React.FC = () => {
                 <path d="M40 0C17.9108 0 0 17.9108 0 40C0 62.0892 17.9108 80 40 80C62.0892 80 80 62.0892 80 40C80 17.9108 62.0928 0 40 0ZM60 17.2227C61.5338 17.2227 62.7773 18.4662 62.7773 20C62.7773 21.5338 61.5338 22.7773 60 22.7773C58.4662 22.7773 57.2227 21.5338 57.2227 20C57.2227 18.4662 58.4662 17.2227 60 17.2227ZM20 62.7809C18.4662 62.7809 17.2227 61.5374 17.2227 60.0036C17.2227 58.4698 18.4662 57.2263 20 57.2263C21.5338 57.2263 22.7773 58.4698 22.7773 60.0036C22.7773 61.5374 21.5338 62.7809 20 62.7809ZM52.6429 51.876C52.2487 54.5852 50.998 57.0794 49.0235 59.0396C46.6296 61.4478 43.4223 62.7809 40 62.7809C33.5925 62.7809 28.2673 58.0398 27.3607 51.8796C14.6999 50.0197 7.22451 45.6477 7.22451 40.0036C7.22451 34.363 14.6999 29.991 27.3607 28.1276C27.7549 25.4184 29.0056 22.9242 30.9801 20.964C33.3739 18.5558 36.5813 17.2227 40.0036 17.2227C43.4259 17.2227 46.6368 18.5558 49.0414 20.9747C51.0016 22.9242 52.2523 25.4148 52.6465 28.124C65.3073 29.9839 72.7827 34.3559 72.7827 40C72.7791 45.6406 65.3037 50.0125 52.6429 51.876Z" fill="url(#paint2_linear_1_714)"/>
                 <defs>
                 <linearGradient id="paint0_linear_1_714" x1="0.466493" y1="75.7757" x2="77.1862" y2="-0.943968" gradientUnits="userSpaceOnUse">
-                <stop offset="0.1" stop-color="#DE1CFF"/>
-                <stop offset="1" stop-color="#32D2FF"/>
+                <stop offset="0.1" stopColor="#DE1CFF"/>
+                <stop offset="1" stopColor="#32D2FF"/>
                 </linearGradient>
                 <linearGradient id="paint1_linear_1_714" x1="4.22665" y1="79.5366" x2="80.9463" y2="2.81692" gradientUnits="userSpaceOnUse">
-                <stop offset="0.1" stop-color="#DE1CFF"/>
-                <stop offset="1" stop-color="#32D2FF"/>
+                <stop offset="0.1" stopColor="#DE1CFF"/>
+                <stop offset="1" stopColor="#32D2FF"/>
                 </linearGradient>
                 <linearGradient id="paint2_linear_1_714" x1="2.34646" y1="77.6559" x2="79.0662" y2="0.93614" gradientUnits="userSpaceOnUse">
-                <stop offset="0.1" stop-color="#DE1CFF"/>
-                <stop offset="1" stop-color="#32D2FF"/>
+                <stop offset="0.1" stopColor="#DE1CFF"/>
+                <stop offset="1" stopColor="#32D2FF"/>
                 </linearGradient>
                 </defs>
             </svg>
@@ -64,10 +64,10 @@ const Login: React.FC = () => {
                         aria-label={passwordVisible ? "Скрыть пароль" : "Показать пароль"}
                     >
                         <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10.2054 9.03845C9.77901 9.75736 8.95086 10.1357 8.12829 9.98734C7.30573 9.83899 6.66192 9.19518 6.51357 8.37262C6.36522 7.55005 6.74355 6.7219 7.46246 6.29553" stroke="#79747F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M12.4978 11.3309C11.348 12.2031 9.94332 12.6732 8.50011 12.6688C6.10871 12.7113 3.89914 11.3971 2.79507 9.27536C2.39847 8.47148 2.39847 7.52884 2.79507 6.72496C3.34723 5.62537 4.22743 4.72453 5.31393 4.14703" stroke="#79747F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M14.1178 9.42324C14.1445 9.37255 14.1793 9.32709 14.2046 9.27543C14.6012 8.47156 14.6012 7.52892 14.2046 6.72504C13.1006 4.60332 10.891 3.28904 8.49957 3.33163C8.34965 3.33163 8.20392 3.35164 8.05591 3.36139" stroke="#79747F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M14.5024 13.3354L3.16431 1.99731" stroke="#79747F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M10.2054 9.03845C9.77901 9.75736 8.95086 10.1357 8.12829 9.98734C7.30573 9.83899 6.66192 9.19518 6.51357 8.37262C6.36522 7.55005 6.74355 6.7219 7.46246 6.29553" stroke="#79747F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M12.4978 11.3309C11.348 12.2031 9.94332 12.6732 8.50011 12.6688C6.10871 12.7113 3.89914 11.3971 2.79507 9.27536C2.39847 8.47148 2.39847 7.52884 2.79507 6.72496C3.34723 5.62537 4.22743 4.72453 5.31393 4.14703" stroke="#79747F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M14.1178 9.42324C14.1445 9.37255 14.1793 9.32709 14.2046 9.27543C14.6012 8.47156 14.6012 7.52892 14.2046 6.72504C13.1006 4.60332 10.891 3.28904 8.49957 3.33163C8.34965 3.33163 8.20392 3.35164 8.05591 3.36139" stroke="#79747F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M14.5024 13.3354L3.16431 1.99731" stroke="#79747F" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                     </button>
                 </div>
--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,6 +1,5 @@
 import './MainPage.css';
 
-// src/MainPage.tsx
 import React, { useState } from 'react';
 
 import Sidebar from '../../components/Sidebar/Sidebar';
@@ -8,6 +7,32 @@ import Sidebar from '../../components/Sidebar/Sidebar';
 const MainPage: React.FC = () => {
   const user1 = { id: 1, name: "Михаил", email: "artem@gmail.com", role: { "Ученик": true }, message: 2, image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" };
   const user2 = { id: 2, name: "Анна", email: "artem@gmail.com", role: { "Ученик": true }, message: 1, image: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" };
+  // Задаем дату окончания события (может быть любая дата в будущем)
+const endDate: Date = new Date('2024-06-30T00:00:00');
+
+// Функция для вычисления оставшегося времени
+function getTimeRemaining(endTime: Date): { days: number, hours: number, minutes: number } {
+  // Текущая дата и время
+  const currentTime: Date = new Date();
+
+  // Разница между текущим временем и временем окончания события
+  let timeDiff: number = endTime.getTime() - currentTime.getTime();
+
+  // Вычисляем дни, часы, минуты и секунды
+  const days: number = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours: number = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes: number = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  
+  // Возвращаем объект с полученными значениями
+  return {
+    days: days,
+    hours: hours,
+    minutes: minutes
+  };
+}
+
+// Получаем оставшееся время до заданной даты
+const remainingTime = getTimeRemaining(endDate);
 
   const [activeUser, setActiveUser] = useState("user1");
 
@@ -28,11 +53,11 @@ const MainPage: React.FC = () => {
             <a className="chat" href="">
               <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="21" cy="24" r="20.5" fill="white" stroke="#7362BC"/>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M27 33.5H23.5C22.5334 33.5 21.75 32.7166 21.75 31.75V27.0833C21.75 26.1167 22.5334 25.3333 23.5 25.3333H30.5C31.4666 25.3333 32.25 26.1167 32.25 27.0833V31.75C32.25 32.7166 31.4666 33.5 30.5 33.5H29.625V35.25L27 33.5Z" fill="#EEEEFF" stroke="#434B74" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M18 29H24C25.657 29 27 27.657 27 26V18C27 16.343 25.657 15 24 15H12C10.343 15 9 16.343 9 18V26C9 27.657 10.343 29 12 29H13.5V32L18 29Z" fill="#FFF1CB" stroke="#434B74" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M18.6768 22.3232C18.7744 22.4209 18.7744 22.5791 18.6768 22.6768C18.5791 22.7744 18.4209 22.7744 18.3232 22.6768C18.2256 22.5791 18.2256 22.4209 18.3232 22.3232C18.4209 22.2256 18.5791 22.2256 18.6768 22.3232" stroke="#434B74" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M22.6768 22.3232C22.7744 22.4209 22.7744 22.5791 22.6768 22.6768C22.5791 22.7744 22.4209 22.7744 22.3232 22.6768C22.2256 22.5791 22.2256 22.4209 22.3232 22.3232C22.4209 22.2256 22.5791 22.2256 22.6768 22.3232" stroke="#434B74" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M14.6768 22.3232C14.7744 22.4209 14.7744 22.5791 14.6768 22.6768C14.5791 22.7744 14.4209 22.7744 14.3232 22.6768C14.2256 22.5791 14.2256 22.4209 14.3232 22.3232C14.4209 22.2256 14.5791 22.2256 14.6768 22.3232" stroke="#434B74" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path fillRule="evenodd" clipRule="evenodd" d="M27 33.5H23.5C22.5334 33.5 21.75 32.7166 21.75 31.75V27.0833C21.75 26.1167 22.5334 25.3333 23.5 25.3333H30.5C31.4666 25.3333 32.25 26.1167 32.25 27.0833V31.75C32.25 32.7166 31.4666 33.5 30.5 33.5H29.625V35.25L27 33.5Z" fill="#EEEEFF" stroke="#434B74" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <path fillRule="evenodd" clipRule="evenodd" d="M18 29H24C25.657 29 27 27.657 27 26V18C27 16.343 25.657 15 24 15H12C10.343 15 9 16.343 9 18V26C9 27.657 10.343 29 12 29H13.5V32L18 29Z" fill="#FFF1CB" stroke="#434B74" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M18.6768 22.3232C18.7744 22.4209 18.7744 22.5791 18.6768 22.6768C18.5791 22.7744 18.4209 22.7744 18.3232 22.6768C18.2256 22.5791 18.2256 22.4209 18.3232 22.3232C18.4209 22.2256 18.5791 22.2256 18.6768 22.3232" stroke="#434B74" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M22.6768 22.3232C22.7744 22.4209 22.7744 22.5791 22.6768 22.6768C22.5791 22.7744 22.4209 22.7744 22.3232 22.6768C22.2256 22.5791 22.2256 22.4209 22.3232 22.3232C22.4209 22.2256 22.5791 22.2256 22.6768 22.3232" stroke="#434B74" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M14.6768 22.3232C14.7744 22.4209 14.7744 22.5791 14.6768 22.6768C14.5791 22.7744 14.4209 22.7744 14.3232 22.6768C14.2256 22.5791 14.2256 22.4209 14.3232 22.3232C14.4209 22.2256 14.5791 22.2256 14.6768 22.3232" stroke="#434B74" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                 <path d="M39.1272 10.3V10.63C39.1272 10.83 38.9572 11 38.7572 11H35.0272C34.8272 11 34.6572 10.83 34.6572 10.63V10.42C34.6572 10.23 34.7672 10.02 34.9972 9.81C35.3572 9.47 35.6672 9.17 35.9172 8.91C36.1672 8.65 36.4372 8.34 36.7372 7.98C37.3272 7.26 37.6372 6.57 37.6372 5.98C37.6372 5.41 37.3172 5.07 36.7472 5.07C36.3772 5.07 36.0572 5.26 35.8072 5.64C35.7272 5.77 35.6172 5.84 35.4672 5.84C35.4072 5.84 35.3472 5.82 35.2772 5.79L35.0072 5.65C34.8772 5.58 34.8072 5.47 34.8072 5.32C34.8072 5.27 34.8172 5.22 34.8472 5.17C35.3172 4.29 35.8872 3.92 36.8772 3.92C37.5072 3.92 37.9872 4.1 38.3372 4.46C38.6872 4.82 38.8572 5.25 38.8572 5.75C38.8572 6.48 38.4972 7.34 38.0972 7.93C37.8972 8.22 37.6572 8.52 37.3772 8.83C37.0872 9.14 36.8672 9.36 36.7172 9.5C36.5572 9.65 36.3872 9.79 36.2172 9.93H38.7572C38.9572 9.93 39.1272 10.1 39.1272 10.3Z" fill="white"/>
               </svg>
               {activeUser === 'user1' && user1.message > 0 && (
@@ -96,9 +121,9 @@ const MainPage: React.FC = () => {
                   </div>
                   <div>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M16.4445 8.44253L20.0019 12L16.4445 15.5575" stroke="#008AFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                      <path d="M9.33191 12L20.0034 12" stroke="#008AFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                      <path d="M7.55432 3.99666C5.58958 3.99666 3.99684 5.5894 3.99684 7.55414L3.99684 16.4458C3.99684 18.4106 5.58958 20.0033 7.55432 20.0033" stroke="#008AFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                      <path d="M16.4445 8.44253L20.0019 12L16.4445 15.5575" stroke="#008AFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                      <path d="M9.33191 12L20.0034 12" stroke="#008AFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                      <path d="M7.55432 3.99666C5.58958 3.99666 3.99684 5.5894 3.99684 7.55414L3.99684 16.4458C3.99684 18.4106 5.58958 20.0033 7.55432 20.0033" stroke="#008AFF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                   </div>
                 </div>
@@ -107,56 +132,72 @@ const MainPage: React.FC = () => {
           </div>
         </header>
         <main>
-          <section className="promo-banner">
-            <p>До 31 декабря любой курс со скидкой 20%</p>
-            <p>До конца года у вас есть уникальная возможность воспользоваться нашей новогодней скидкой 20% на любой курс!</p>
-            <button>Button</button>
-          </section>
-          <section className="lessons-balance">
-            <h2>Баланс занятий</h2>
-            <ul>
-              <li>
-                <span>Ментальная Арифметика</span>
-                <span>32</span>
-              </li>
-              <li>
-                <span>Программирование</span>
-                <span>0</span>
-              </li>
-              <li>
-                <span>Скорочтение</span>
-                <span>4</span>
-              </li>
-            </ul>
-            <button>Button</button>
-          </section>
-          <section className="upcoming-lessons">
-            <h2>Ближайшие уроки</h2>
-            <ul>
-              <li>
-                <span>Ментальная Арифметика</span>
-                <span>14:00-14:25</span>
-                <span>Белкина Инна</span>
-                <button>Button</button>
-                <button>Button</button>
-              </li>
-              <li>
-                <span>Программирование</span>
-                <span>11:00-11:11</span>
-                <span>Животовская Оксана</span>
-                <button>Button</button>
-                <button>Button</button>
-              </li>
-              <li>
-                <span>Скорочтение</span>
-                <span>09:00-09:45</span>
-                <span>Мина Елена</span>
-                <button>Button</button>
-                <button>Button</button>
-              </li>
-            </ul>
-            <button>Button</button>
-          </section>
+          <div className='promo'>
+            <section className="promo-banner">
+              <div>
+                <p className="promo-banner-title">До 31 декабря любой курс со скидкой 20%</p>
+                <p className="promo-banner-content">До конца года у вас есть уникальная возможность воспользоваться нашей новогодней скидкой 20% на любой курс!</p>
+              </div>
+              <img className='promo-banner-img' src="/src/assets/SF_21.svg" alt="shop" />
+            </section>
+            <section className="upcoming-banner">
+              <p className="upcoming-banner-title">Следующее занятие начнется через:</p>
+              <p className='timer'>{remainingTime.days}<small className='small'>д</small> {remainingTime.hours}<small className='small'>ч</small> {remainingTime.minutes}<small className='small'>мин</small></p>
+              <button className='upcoming-btn'>Button</button>
+            </section>
+            <section className="pink-banner">
+              <h2>Title </h2>
+              <p>content</p>
+            </section>
+          </div>
+          <div className="content">
+            <section className="lessons-balance">
+              <h2>Баланс занятий</h2>
+              <ul>
+                <li>
+                  <span>Ментальная Арифметика</span>
+                  <span>32</span>
+                </li>
+                <li>
+                  <span>Программирование</span>
+                  <span>0</span>
+                </li>
+                <li>
+                  <span>Скорочтение</span>
+                  <span>4</span>
+                </li>
+              </ul>
+              <button>Button</button>
+            </section>
+            <section className="upcoming-lessons">
+              <h2>Ближайшие уроки</h2>
+              <ul>
+                <li>
+                  <span>Ментальная Арифметика</span>
+                  <span>14:00-14:25</span>
+                  <span>Белкина Инна</span>
+                  <button>Button</button>
+                  <button>Button</button>
+                </li>
+                <li>
+                  <span>Программирование</span>
+                  <span>11:00-11:11</span>
+                  <span>Животовская Оксана</span>
+                  <button>Button</button>
+                  <button>Button</button>
+                </li>
+                <li>
+                  <span>Скорочтение</span>
+                  <span>09:00-09:45</span>
+                  <span>Мина Елена</span>
+                  <button>Button</button>
+                  <button>Button</button>
+                </li>
+              </ul>
+              <button>Button</button>
+            </section>
+          </div>
+          
         </main>
       </div>
     </div>
